c09431757f7e8248b55ed3bb3a7c60fc101ba47a
diana nica <themindhelp@gmail.com>
2023-06-20 09:46:15 +0100

    code refactoring

--- minesweeper.js
+++ minesweeper.js
@@ -93,43 +93,39 @@ export function checkLose(board) {
 }
 
 function getMinePositions(boardSize, numberOfMines) {
-    const positions = []
+  const positions = [];
 
-     while (positions.length < numberOfMines) {
-        const position = {
-            x: randomNumber(boardSize)
-            y: randomNumber(boardSize)
-        
-        }
+  while (positions.length < numberOfMines) {
+    const position = {
+      x: randomNumber(boardSize),
+      y: randomNumber(boardSize),
+    };
 
-        if (!positions.some(positionMatch.bind(null, position))) {
-            positions.push(position)
-        }
-     }
-
-     return positions
-
+    if (!positions.some(positionMatch.bind(null, position))) {
+      positions.push(position);
     }
+  }
 
-    function positionMatch(a,b) {
-        return a.x === b.x && a.y === b.y
+  return positions;
+}
+
+function positionMatch(a, b) {
+  return a.x === b.x && a.y === b.y;
+}
+
+function randomNumber(size) {
+  return Math.floor(Math.random() * size);
+}
+
+function nearbyTiles(board, { x, y }) {
+  const tiles = [];
+
+  for (let xOffset = -1; xOffset <= 1; xOffset++) {
+    for (let yOffset = -1; yOffset <= 1; yOffset++) {
+      const tile = board[x + xOffset]?.[y + yOffset];
+      if (tile) tiles.push(tile);
     }
+  }
 
-    function randomNumber(size) {
-        return Math.floor(Math.random() * size)
-
-    }
-
-
-    function nearbyTiles(board, {x,y}) {
-        const tiles = []
-
-        for (let xOffset = -1; xOffset <= 1; xOffset++) {
-            for (let yOffset = -1; yOffset <=1; yOffset++) {
-                const tile =board[x + xOffset]?.[y + yOffset]
-                if (tile) tiles.push(tile)
-            }
-        }
-
-        return tiles
-    }
+  return tiles;
+}
--- script.js
+++ script.js
@@ -1,82 +1,74 @@
 // Display UI
 
 import {
-    TILE_STATUSES,
-    createBoard,
-    markTile,
-    revealTile,
-    checkWin,
-    checkLose, 
+  TILE_STATUSES,
+  createBoard,
+  markTile,
+  revealTile,
+  checkWin,
+  checkLose,
+} from "./minesweeper.js";
 
-} from "./minesweeper.js"
+const BOARD_SIZE = 10;
+const NUMBER_OF_MINES = 3;
 
-const BOARD_SIZE =10
-const NUMBER_OF_MINES = 3
+const board = createBoard(BOARD_SIZE, NUMBER_OF_MINES);
+const boardELement = document.querySelector(".board");
+const minesLeftText = document.querySelector("[data-mine-count]");
+const messageText = document.querySelector(".subtext");
 
-const board = createBoard (BOARD_SIZE, NUMBER_OF_MINES)
-const boardELement = document.querySelector(."board")
-const minesLeftText = document.querySelector("[data-mine-count]")
-const messageText = document.querySelector(".subtext")
+board.forEach((row) => {
+  row.forEach((tile) => {
+    boardELement.append(tile.element);
+    tile.element.addEventListener("click", () => {
+      revealTile(board, tile);
+      checkGameEnd();
+    });
+    tile.element.addEventListener("contextmenu", (e) => {
+      e.preventDefault();
+      markTile(tile);
+      listMinesLeft();
+    });
+  });
+});
 
-
-board.forEach(row => {
-    row.forEach(tile =>{
-        boardELement.append(tile.element)
-        tile.element.addEventListener("click", () => {
-            revealTile(board, tile)
-            checkGameEnd()
-        })
-        tile.element.addEventListener("contextmenu", e => {
-            e.preventDefault()
-            markTile(tile)
-            listMinesLeft()
-        })
-    })
-})
-
-boardELement.style.setProperty("--size", BOARD_SIZE)
-minesLeftText.textContent = NUMBER_OF_MINES
+boardELement.style.setProperty("--size", BOARD_SIZE);
+minesLeftText.textContent = NUMBER_OF_MINES;
 
 function listMinesLeft() {
-    const markedTilesCount = board.reduce((count, row) => {
-        return (
-            count + row.filter(tile => tile.status === TILE_STATUSES.MARKED).length
-        )
-    }, 0)
-    
-    minesLeftText.textContent = NUMBER_OF_MINES - markedTilesCount
+  const markedTilesCount = board.reduce((count, row) => {
+    return (
+      count + row.filter((tile) => tile.status === TILE_STATUSES.MARKED).length
+    );
+  }, 0);
+
+  minesLeftText.textContent = NUMBER_OF_MINES - markedTilesCount;
 }
 
-
 function checkGameEnd() {
-    const win = checkWin(board)
-    const lose = checkLose(board)
+  const win = checkWin(board);
+  const lose = checkLose(board);
 
-    if (win || lose) {
-        boardELement.addEventListener("click", stopProp, {capture: true})
-        boardELement.addEventListener("contextmenu", stopProp, {capture: true})
+  if (win || lose) {
+    boardELement.addEventListener("click", stopProp, { capture: true });
+    boardELement.addEventListener("contextmenu", stopProp, { capture: true });
+  }
 
-    }
+  if (win) {
+    messageText.textContent = "You Win";
+  }
 
-    if (win) {
-        messageText.textContent = "You Win"
-    }
-
-    if (lose) {
-        messageText.textContent = "You Lose"
-        board.forEach(row => {
-            row.forEach(tile => {
-                if(tile.status === TILE_STATUSES.MARKED) markTile(tile)
-                if (tile.mine) revealTile(board, tile)
-
-            })
-        })
-    }
-     
-     
-
+  if (lose) {
+    messageText.textContent = "You Lose";
+    board.forEach((row) => {
+      row.forEach((tile) => {
+        if (tile.status === TILE_STATUSES.MARKED) markTile(tile);
+        if (tile.mine) revealTile(board, tile);
+      });
+    });
+  }
 }
 
 function stopProp(e) {
-    e.stopImmediatePropagation()
+  e.stopImmediatePropagation();
 }
